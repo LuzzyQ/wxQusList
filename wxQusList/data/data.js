af826d9595938661f87bf1c6e7f26dd81fcb2aa9
LuzzyQ <qyyll1999@163.com>
2018-08-10 14:47:43 +0800

    Add files via upload
    a template of question list

---
+++ wxQusList/data/data.js
@@ -0,0 +1,13 @@
+var QuestionList=[{
+  index:0,
+  title:"郑淏南是笨蛋吗？",
+  optionlist:['A.是','B.当然是','C.那还用说']
+},
+{
+  index:1,
+  title:"今天郑淏南起床了吗？",
+  optionlist:['A.没有','B.还睡着','C.你看他回你了吗？']
+}]
+module.exports={
+  QuestionList:QuestionList
+}
